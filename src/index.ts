a40037adadc05521ed631155e9b58e7cc1edc9c4
Ana Cantarelli <accffers@gmail.com>
2023-07-11 21:27:11 -0300

    feat: server express running

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Gremio vai sair campeao');
+import express, { response } from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.get('/gremiovaisercampeao', (request, response) => {
+    response.send('Atenção terraquinos')
+})
+
+app.listen(port, () => console.log(`listening on port ${port}! `))
